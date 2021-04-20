75ee6a27728387710a825edf600f2f59eb604727
Melvin <31448155+melvio@users.noreply.github.com>
2021-04-20 10:49:14 +0200

    missing element from permutation

---
+++ src/003-timecomplexity/permmissingelem-test.js
@@ -0,0 +1,18 @@
+import assert from "assert";
+import {findMissingElement, sumArray} from "./permmissingelem.js";
+
+function testFindMissingElement() {
+    assert(findMissingElement([1, 3]) === 2);
+    assert(findMissingElement([1, 2, 3, 5, 4, 7]) === 6);
+    assert(findMissingElement([1, 2, 3, 4, 7, 8, 6, 10, 9, 11, 13, 12]) === 5);
+}
+
+function testSumArray() {
+    assert(sumArray([]) === 0);
+    assert(sumArray([1]) === 1);
+    assert(sumArray([1, -1]) === 0);
+    assert(sumArray([1, 2, 3]) === 6);
+}
+
+testSumArray();
+testFindMissingElement();
---
+++ src/003-timecomplexity/permmissingelem.js
@@ -0,0 +1,27 @@
+
+export function findMissingElement(arr) {
+    let n = arr.length;
+    let nPlusOne = n + 1;
+
+    // <https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF>
+    // But instead of dividing by 2, use shift by 1 because we don't want floating point errors.
+    // We can safely shift because we multiply a positive odd and even natural number.
+    let expectedTotal = nPlusOne * (nPlusOne + 1) >> 1;
+
+    return expectedTotal - sumArray(arr);
+}
+
+
+export function sumArray(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+    if (arr.length === 1) {
+        return arr[0];
+    }
+
+    return arr.reduce((x, y) => {
+        return x + y;
+    });
+}
+
